f715bd8a4aaf4338b167a13a422854dfd0e71d0b
Furkann35 <yucelfurkan124@gmail.com>
2021-05-21 20:42:53 +0300

    Yeni core yazıldı sistemlere entegre edildi.

--- DataAccess/InMemory/InMemoryProductDal.cs
+++ DataAccess/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,27 +12,27 @@ namespace DataAccess.Concrete.InMemory
     public class InMemoryProductDal : IProductDal
     {
         List<Product> _products;
-
         public InMemoryProductDal()
-        { 
-            //Oracle,Sql Server,Postgres,MongoDb
-            _products = new List<Product> { 
-                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
-                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
-                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
-                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
-                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+        {
+            //Oracle,Sql Server, Postgres , MongoDb
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1}
             };
         }
         public void Add(Product product)
         {
-         Console.WriteLine();
+            _products.Add(product);
         }
 
         public void Delete(Product product)
         {
-            //LINQ
-            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            //LINQ - Language Integrated Query
+            //Lambda
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
 
             _products.Remove(productToDelete);
         }
@@ -40,8 +41,6 @@ namespace DataAccess.Concrete.InMemory
         {
             return _products;
         }
-
-
 
         public void Update(Product product)
         {
@@ -52,6 +51,7 @@ namespace DataAccess.Concrete.InMemory
             productToUpdate.UnitPrice = product.UnitPrice;
             productToUpdate.UnitsInStock = product.UnitsInStock;
         }
+
         public List<Product> GetAllByCategory(int categoryId)
         {
             return _products.Where(p => p.CategoryId == categoryId).ToList();
@@ -63,6 +63,11 @@ namespace DataAccess.Concrete.InMemory
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
         {
             throw new NotImplementedException();
         }
