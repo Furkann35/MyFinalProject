eccf8f86f32b0703ae48751a881dc2e72dfdaf39
Furkann35 <yucelfurkan124@gmail.com>
2021-05-24 23:34:18 +0300

    WebAPI eklendi.Postman ile testler yapıldı.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -20,7 +20,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public IResult Add(Product product)
+     public IResult Add(Product product)
         {
             //business codes
             if (product.ProductName.Length<2)
@@ -35,7 +35,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 20)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
